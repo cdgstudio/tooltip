33000e98fb4c0ef281c0cd5643f0821e3f1c6d91
Adrian Bury <adrian.jakub.bury@gmail.com>
2023-06-27 09:32:22 +0200

    Upgrade to ng16

---
+++ projects/tooltip/index.ts
@@ -0,0 +1,2 @@
+export { TooltipModule } from './src/tooltip.module';
+export { TooltipDirective } from './src/tooltip.directive';
--- src/app/tooltip/index.ts
+++
@@ -1,2 +0,0 @@
-export { TooltipModule } from './tooltip.module';
-export { TooltipDirective } from './tooltip.directive';
