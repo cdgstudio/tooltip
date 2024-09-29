ddfa8a1e3a5008b5bb5c3ec7ca323e303122ccec
addun <adrian.jakub.bury@gmail.com>
2024-09-29 12:03:08 +0200

    Use syntax flow

--- projects/tooltip/src/tooltip-container/tooltip-container.component.ts
+++ projects/tooltip/src/tooltip-container/tooltip-container.component.ts
@@ -1,20 +1,20 @@
-import { ChangeDetectionStrategy, Component, Inject, InjectionToken, TemplateRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, InjectionToken, TemplateRef } from "@angular/core";
 
 export type TooltipData = string | TemplateRef<void>;
-export const TOOLTIP_DATA = new InjectionToken<TooltipData>('Data to display in tooltip');
+export const TOOLTIP_DATA = new InjectionToken<TooltipData>("Data to display in tooltip");
 
 @Component({
-  selector: 'app-tooltip-container',
-  templateUrl: './tooltip-container.component.html',
-  styleUrls: ['./tooltip-container.component.scss'],
+  selector: "app-tooltip-container",
+  templateUrl: "./tooltip-container.component.html",
+  styleUrls: ["./tooltip-container.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TooltipContainerComponent {
-  get asString(): string | false {
-    return typeof this.tooltipData === 'string' ? this.tooltipData : false;
+  get isString(): string | false {
+    return typeof this.tooltipData === "string" ? this.tooltipData : false;
   }
 
-  get asTemplate(): TemplateRef<void> | false {
+  get isTemplate(): TemplateRef<void> | false {
     return this.tooltipData instanceof TemplateRef ? this.tooltipData : false;
   }
 
